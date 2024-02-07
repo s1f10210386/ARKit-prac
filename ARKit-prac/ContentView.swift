9e83934059e7383100ed09c3f4d20ad0003d50b4
hotaka <s1f102103864@iniad.org>
2024-02-07 14:01:03 +0900

    feat : Added the ability to change color when tapped

--- ARKit-prac/ContentView.swift
+++ ARKit-prac/ContentView.swift
@@ -9,16 +9,25 @@ import SwiftUI
 import RealityKit
 
 struct ContentView : View {
+    @State var isCubeRed = false
+    
     var body: some View {
-        ARViewContainer().edgesIgnoringSafeArea(.all)
+        //これで２つのisCubeRedを連動
+        ARViewContainer(isCubeRed: $isCubeRed)
+            .edgesIgnoringSafeArea(.all)
+            .onTapGesture{
+                isCubeRed = true
+            }
     }
 }
 
 struct ARViewContainer: UIViewRepresentable {
+    @Binding var isCubeRed: Bool
+    
     func makeUIView(context: Context) -> ARView {
         
         let arView = ARView(frame: .zero)
-
+        
         //オブジェクトの形状
         let mesh = MeshResource.generateBox(size: 0.1, cornerRadius: 0.005)
         //オブジェクトの表面の外観
@@ -26,20 +35,31 @@ struct ARViewContainer: UIViewRepresentable {
         //
         let model = ModelEntity(mesh: mesh, materials: [material])
         model.transform.translation.y = 0.05
-
+        
         //AnchorEntityはオブジェクトをどこに置くのか指定するためのもの
         let anchor = AnchorEntity(.plane(.horizontal, classification: .any, minimumBounds: SIMD2<Float>(0.2, 0.2)))
         
         //anchorの子階層に立方体のモデルを追加
         anchor.children.append(model)
-
+        
         //ARViewのsceneにアンカー追加
         arView.scene.anchors.append(anchor)
-
+        
         return arView
         
     }
     
-    func updateUIView(_ uiView: ARView, context: Context) {}
+    func updateUIView(_ uiView: ARView, context: Context) {
+        if isCubeRed {
+            let mesh = MeshResource.generateBox(size: 0.1, cornerRadius: 0.005)
+            let material = SimpleMaterial(color: .red,roughness: 0.15, isMetallic: true)
+            let newmodel = ModelEntity(mesh: mesh, materials:  [material])
+            
+            //uiViewはarViewのこと。sceneの中のアンカー配列にアクセス。(今回平面１個のみ追加したので0)。さらに平面アンカーには立方体しか追加してないからこれも0。
+            //これで立方体にアクセス！これをgrayのモデルからredのモデルに置き換える
+            uiView.scene.anchors[0].children[0] = newmodel
+            
+        }
+    }
     
 }
