903bdd9960a6257f463dbc2cc353a73999072d9d
hotaka <s1f102103864@iniad.org>
2024-02-12 02:10:38 +0900

    feat : Object placement using ARGeoTrackingConfiguration is possible!

---
+++ ARKit-prac/ARView+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  ARView+Extension.swift
+//  ARKit-prac
+//
+//  Created by 金澤帆高 on 2024/02/12.
+//
+
+import SwiftUI
+
+struct ARView_Extension: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ARView_Extension()
+}
---
+++ ARKit-prac/Coordinator.swift
@@ -0,0 +1,18 @@
+//
+//  Coordinator.swift
+//  ARKit-prac
+//
+//  Created by 金澤帆高 on 2024/02/12.
+//
+
+import SwiftUI
+
+struct Coordinator: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Coordinator()
+}
