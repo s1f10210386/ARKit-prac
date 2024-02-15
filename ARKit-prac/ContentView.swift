9d9ab7978291dfdb300930b949715d5d876f5f8f
hotaka <s1f102103864@iniad.org>
2024-02-15 23:27:58 +0900

    feat:SceneKitのところをRealityKitで書き直せた

--- ARKit-prac/ContentView.swift
+++ ARKit-prac/ContentView.swift
@@ -5,13 +5,35 @@
 //  Created by 金澤帆高 on 2024/02/15.
 //
 
+//import SwiftUI
+//
+//struct ContentView: View {
+//    @StateObject private var viewModel = ARViewModel()
+//
+//    var body: some View {
+//        VStack {
+//            ARViewContainer(viewModel: viewModel)
+//                .edgesIgnoringSafeArea(.all)
+//
+//            Button("Start AR Session") {
+//                viewModel.startARSession()
+//            }
+//        }
+//    }
+//}
+
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var viewModel = ARViewModel()
+
     var body: some View {
-            ARViewContainer()
+        VStack {
+            ARViewContainer(viewModel: viewModel)
                 .edgesIgnoringSafeArea(.all)
-        
+            Button("Start AR Session") {
+                viewModel.startARSession()
+            }
+        }
     }
 }
-
